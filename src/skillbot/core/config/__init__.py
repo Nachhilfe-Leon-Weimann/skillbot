7a72de9c5ca411aa1bf4515778986b10d4bad11d
Leon Weimann <leonweimann@icloud.com>
2026-01-04 18:51:14 +0100

    ref: move config into subpackage

---
+++ src/skillbot/core/config/__init__.py
@@ -0,0 +1,15 @@
+from .settings import (
+    DatabaseSettings,
+    DiscordSettings,
+    LoggingSettings,
+    Settings,
+    get_settings,
+)
+
+__all__ = [
+    "DatabaseSettings",
+    "DiscordSettings",
+    "LoggingSettings",
+    "Settings",
+    "get_settings",
+]
